250c53f1e3b8cd1c668038d6679236a7ea1c2d5c
navajoted <tsanders@navajomfg.com>
2014-03-22 17:57:20 -0600

    RockyMountainOverstock_KoleImports Integration

---
+++ app/code/local/RockyMountainOverstock/KoleImports/Controllers/IndexControllers.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tsanders
+ * Date: 3/22/14
+ * Time: 5:22 PM
+ */
+class RockyMountainOverstock_KoleImports_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction ()
+    {
+        echo 'test index';
+    }
+    public function mamethodeAction ()
+    {
+        echo 'test mymethod';
+    }
+}
