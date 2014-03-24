94ca82afa0e380576c65878c3585444915a7b364
navajoted <tsanders@navajomfg.com>
2014-03-24 15:23:28 -0600

    RockyMountainOverstock_KoleImports Integration

---
+++ pathchecker.php
@@ -0,0 +1,12 @@
+
+/**
+ * Created by PhpStorm.
+ * User: tsanders
+ * Date: 3/24/14
+ * Time: 2:28 PM
+ * path checker file for simple FTP navigation
+/
+<?php
+echo realpath(dirname(__FILE__)) . "\n";
+echo $_SERVER['PHP_SELF'] . "\n";
+?>
